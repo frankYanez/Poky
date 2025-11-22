1aadf78180b9c8fa2cd794c474706abc86d1544b
Francisco Yañez <frankyanez9413@gmail.com>
2025-11-22 15:38:56 -0300

    Creating landing sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import GradientText from "@/components/GradientText/GradientText";
+import { Hero } from "@/src/features/landing/components/Hero";
+import { ProblemSection } from "@/src/features/landing/components/ProblemSection";
+import { SolutionSection } from "@/src/features/landing/components/SolutionSection";
+
 export default function Home() {
-  return <main className="relative flex flex-col gap-12 pb-16 pt-12"></main>;
+  return (
+    <main className="container flex min-h-screen flex-col gap-24">
+      <Hero />
+      <ProblemSection />
+      <GradientText
+        colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+        animationSpeed={10}
+        showBorder={false}
+        className="text-4xl text-center "
+      >
+        Protocolo de Validacion de conocimiento
+      </GradientText>
+      <SolutionSection />
+    </main>
+  );
 }
